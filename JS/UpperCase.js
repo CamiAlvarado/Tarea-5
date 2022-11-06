7e51aca2de62f23f3a2bca78f9f79d3cba81b71f
CamiAlvarado <camialvarado.f92@gmail.com>
2022-11-05 22:12:51 -0300

    Se cambia nombre de los archivos y let.

--- JS/UpperCase.js
+++ JS/UpperCase.js
@@ -1,5 +1,5 @@
 //Retornar otro arreglo con todo a mayúsculas.
 
-let pokemones = ["Pikachu", "Charmander", "Bulbasaur", "Squirtle"]
-let pokemayus = pokemones.map(item => item.toUpperCase())
+let poke = ["Pikachu", "Charmander", "Bulbasaur", "Squirtle"]
+let pokemayus = poke.map(item => item.toUpperCase())
 console.log(pokemayus)
